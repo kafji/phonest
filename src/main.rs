50b318431f3a35f2e23c9f6d3a1c8da8244abd43
Kafji <k@kafji.net>
2020-09-21 11:07:38 +0700

    Accept text input from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use ::spellit::*;
+use anyhow::{Context, Result};
+use std::{
+    env,
+    io::{self, Read, Write},
+};
 
-fn main() -> anyhow::Result<()> {
-    let matches = clap::App::new(env!("CARGO_PKG_NAME"))
-        .setting(clap::AppSettings::ArgRequiredElseHelp)
+fn main() -> Result<()> {
+    let mut app = clap::App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .arg(
@@ -10,10 +14,33 @@ fn main() -> anyhow::Result<()> {
                 .required(true)
                 .value_name("TEXT")
                 .help("A literal text to spell in phonetic alphabet"),
-        )
-        .get_matches();
+        );
 
-    let text = matches.value_of_lossy("input").unwrap();
+    let text = match app.get_matches_from_safe_borrow(env::args_os()) {
+        Ok(matches) => (*matches.value_of_lossy("input").unwrap()).to_owned(),
+        Err(error) => match error.kind {
+            clap::ErrorKind::MissingRequiredArgument => {
+                if atty::is(atty::Stream::Stdin) {
+                    let stdout = io::stdout();
+                    let mut stdout = stdout.lock();
+                    app.write_help(&mut stdout)
+                        .with_context(|| "Failed to write output to stdout.")?;
+                    stdout
+                        .write_all(b"\n\n")
+                        .with_context(|| "Failed to write output to stdout.")?;
+                    return Ok(());
+                }
+                let mut input = String::new();
+                io::stdin()
+                    .read_to_string(&mut input)
+                    .with_context(|| "Failed to read input from stdin.")?;
+                let input = input.trim().to_owned();
+                input
+            }
+            _ => return Err(anyhow::Error::from(error)),
+        },
+    };
+
     let phonetics = phonetic_alphabets_of(&text);
 
     let characters = text.chars().zip(phonetics);
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,11 +1,23 @@
-use assert_cmd::prelude::*;
-use std::process::Command;
+use assert_cmd::Command;
 
 #[test]
-fn example() {
+fn trivial() {
     let output = Command::cargo_bin("spellit")
         .unwrap()
         .arg("Hello!")
+        .unwrap();
+    assert!(output.status.success());
+    assert_eq!(
+        "H -> Hotel\ne -> Echo\nl -> London\nl -> London\no -> Oscar\n! -> \n",
+        String::from_utf8(output.stdout).unwrap()
+    );
+}
+
+#[test]
+fn from_stdin() {
+    let output = Command::cargo_bin("spellit")
+        .unwrap()
+        .write_stdin("Hello!")
         .unwrap();
     assert!(output.status.success());
     assert_eq!(
