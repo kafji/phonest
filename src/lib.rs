d862c0439e3af234ffbfc1d4c076b028f725ed31
Kafji <k@kafji.net>
2021-04-16 12:31:48 +0700

    Add support for numbers

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
-const PHONETIC_ALPHABETS: [&str; 26] = [
+// Based on table in https://en.wikipedia.org/wiki/NATO_phonetic_alphabet#Pronunciation_of_code_words
+
+const PHONETIC_LETTERS: [&str; 26] = [
     "Alfa", "Bravo", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India", "Juliet",
     "Kilo", "London", "Mike", "November", "Oscar", "Papa", "Quebec", "Romeo", "Sierra", "Tango",
     "Uniform", "Victor", "Whiskey", "X-ray", "Yankee", "Zulu",
+];
+
+const PHONETIC_NUMBERS: [&str; 10] = [
+    "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
 ];
 
 #[inline]
@@ -9,11 +15,17 @@ pub fn phonetic_alphabets_of(text: &str) -> Vec<Option<&str>> {
     text.chars()
         .map(|c| {
             let d = c as u8;
-            if d >= 97 && d <= 122 {
-                return Some(PHONETIC_ALPHABETS[(d - 97) as usize]);
+            // Numbers
+            if d >= 48 && d <= 57 {
+                return Some(PHONETIC_NUMBERS[(d - 48) as usize]);
             }
+            // Non-capitalized letters
+            if d >= 97 && d <= 122 {
+                return Some(PHONETIC_LETTERS[(d - 97) as usize]);
+            }
+            // Capitalized letters
             if d >= 65 && d <= 90 {
-                return Some(PHONETIC_ALPHABETS[(d - 65) as usize]);
+                return Some(PHONETIC_LETTERS[(d - 65) as usize]);
             }
             None
         })
@@ -40,9 +52,16 @@ mod tests {
 
     #[test]
     fn test_ascii_numbers() {
-        let txt = "1337";
+        let txt = "1234";
         let phonetic = phonetic_alphabets_of(txt);
-        assert_eq!(phonetic, (0..4).map(|_| None).collect::<Vec<_>>());
+        assert_eq!(
+            phonetic,
+            ["One", "Two", "Three", "Four"]
+                .iter()
+                .cloned()
+                .map(|x| Some(x))
+                .collect::<Vec<_>>()
+        );
     }
 
     #[test]
--- tests/tests.rs
+++ tests/tests.rs
@@ -4,11 +4,11 @@ use assert_cmd::Command;
 fn trivial() {
     let output = Command::cargo_bin("spellit")
         .unwrap()
-        .arg("Hello!")
+        .arg("Hello 1337!")
         .unwrap();
     assert!(output.status.success());
     assert_eq!(
-        "H -> Hotel\ne -> Echo\nl -> London\nl -> London\no -> Oscar\n! -> \n",
+        "H -> Hotel\ne -> Echo\nl -> London\nl -> London\no -> Oscar\n  -> \n1 -> One\n3 -> Three\n3 -> Three\n7 -> Seven\n! -> \n",
         String::from_utf8(output.stdout).unwrap()
     );
 }
@@ -17,11 +17,11 @@ fn trivial() {
 fn from_stdin() {
     let output = Command::cargo_bin("spellit")
         .unwrap()
-        .write_stdin("Hello!")
+        .write_stdin("Hello 1337!")
         .unwrap();
     assert!(output.status.success());
     assert_eq!(
-        "H -> Hotel\ne -> Echo\nl -> London\nl -> London\no -> Oscar\n! -> \n",
+        "H -> Hotel\ne -> Echo\nl -> London\nl -> London\no -> Oscar\n  -> \n1 -> One\n3 -> Three\n3 -> Three\n7 -> Seven\n! -> \n",
         String::from_utf8(output.stdout).unwrap()
     );
 }
